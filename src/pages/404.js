f8468ff1ebcb5196bc50c9815a3c04c2a6fccacc
Shivansh <shivansh.bakshi@gmail.com>
2022-10-28 02:44:01 +0530

    Finished 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,27 +1,38 @@
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Typewriter from "typewriter-effect";
-import { Container404, Message404, MessageFooter, MessageHeader } from "../sc/404";
+import { Container404, Message, Message404, MessageFooter, MessageHeader } from "../sc/404";
 
 const NotFoundPage = () => {
     return(
         <Container404>
-            <MessageHeader>OOPS! Looks like you've hit a</MessageHeader>
-            <Message404>
-                <Typewriter 
-                    onInit={(typewriter) => {
-                        typewriter
-                        .typeString("404")
-                        .changeDelay(50)
-                        .pauseFor(100)
-                        .start()
-                    }}
-                    options={{
-                        cursor: "",
-                    }}
-                />
-            </Message404>
-            <MessageFooter>Try going back, or click <Link to="/">here</Link> to go Home</MessageFooter>
+            <StaticImage 
+                src="../images/404.png" 
+                alt="Fishing"
+                style={{
+                    left: -10,
+                }}
+            />
+            <Message>
+                <MessageHeader>OOPS! Looks like you've hit a</MessageHeader>
+                <Message404>
+                    <Typewriter 
+                        onInit={(typewriter) => {
+                            typewriter
+                            .typeString("404")
+                            .changeDelay(50)
+                            .pauseFor(100)
+                            .start()
+                        }}
+                        options={{
+                            cursor: "",
+                        }}
+                    />
+                </Message404>
+                <MessageFooter>Try going back, or click <Link to="/">here</Link> to go Home</MessageFooter>
+            </Message>
+
         </Container404>
     )
 }
--- src/sc/404.js
+++ src/sc/404.js
@@ -5,23 +5,38 @@ export const Container404 = styled.div`
     ${fonts}
 
     font-family: 'Source Code Pro';
+    margin: 0;
+    padding: 0;
+    position: relative;
+`
+export const Message = styled.div`
+    gap: 2rem;
+    position: absolute;
+    top: 0;
+    flex-direction: column;
+    margin: 2rem 0;
+    left: 0;
+    right: 0;
     text-align: center;
-
 `
 
 export const MessageHeader = styled.h2`
-    font-size: calc(16px + 1vw);
+    font-size: calc(23px + 1vw);
     font-weight: 700vh;
 `
 
 export const Message404 = styled.div`
     height: 100%;
     width: 100%;
-    font-size: calc(16px + 20vw);
+    font-size: calc(32px + 25vw);
     line-height: calc(1.1em + 1vw);
 `
 
 export const MessageFooter = styled.h4`
+    position: fixed;
     font-size: calc(16px + 1vw);
     color: black;
+    left: 0;
+    right: 0;
+    bottom: 0;
 `
