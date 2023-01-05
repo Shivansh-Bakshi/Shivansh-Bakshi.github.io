0c0add3d03c38982e02bf36911e5d68159bf034c
Shivansh <shivansh.bakshi@gmail.com>
2023-01-05 17:14:08 +0530

    Added outside click

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,19 @@ import { ExposeFooterIcon, ExposeFooterIconContainer, FooterContainer, MainFoote
 import UpIcon from '../images/up.svg';
 import DownIcon from '../images/down.svg';
 import { transitionEffects } from '../styles/constants';
+import { useIsOutsideClick } from '../utils/hooks';
 
 const Footer = () => {
     const [showFooter, setShowFooter] = React.useState(false);
+    const wrapperRef = React.useRef(null);
+
+    const handleOutsideClick = () => {
+        if (showFooter) {
+            setShowFooter(false);
+        }
+    }
+
+    useIsOutsideClick(wrapperRef, handleOutsideClick);
 
     const handleShowFooter = () => {
         setShowFooter(!showFooter);
@@ -23,7 +33,7 @@ const Footer = () => {
 
     return(
         <FooterContainer>
-            <ExposeFooterIconContainer onClick={handleShowFooter}>
+            <ExposeFooterIconContainer onClick={handleShowFooter} ref={wrapperRef}>
                 <ExposeFooterIcon src={showFooter ? DownIcon : UpIcon} alt='Arrow' />
             </ExposeFooterIconContainer>
             <MainFooter initial="initial" animate="animate" variants={footer}>
